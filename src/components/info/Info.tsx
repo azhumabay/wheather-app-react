e8b6412fd7ad989ab5cbb60afab0a1279e5d5244
azhumabay <zhuma.2001ali@gmail.com>
2024-12-13 17:11:07 +0500

    added main info

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -1,10 +1,16 @@
 import { FC } from "react";
-import info from "./Info.module.css";
+import info from "./Info.module.scss";
+import cloudy from "@icons/cloudy.svg";
 
 const Info: FC = () => {
   return (
-    <div>
-      <h1 className={info.title}>16&deg;</h1>
+    <div className={info.wrapper}>
+      <h1>16&deg;</h1>
+      <div className={info.text}>
+        <h2>London</h2>
+        <p>06:09 - Monday, 9 Sep'23</p>
+      </div>
+      <img src={cloudy} alt="cloudy" />
     </div>
   );
 };
